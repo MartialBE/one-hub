2109fb5f3cf75d14d3b8ef40ecdf8954e03a273b
Cheng YiHONG <2120490235@zufedfc.edu.cn>
2024-06-06 23:47:41 +0800

    ✨ feat: 添加更多新手引导

--- web/src/ui-component/tutorialButton.js
+++ web/src/ui-component/tutorialButton.js
@@ -11,25 +11,69 @@ export default function TutorialButton() {
   const theme = useTheme();
 
   useEffect(() => {
-    setSteps([
-      {
-        element: document.querySelector('a[href="/panel/topup"]'),
-        intro: '这是充值入口.',
-        position: 'right'
-      },
-      {
-        element: document.querySelector('a[href="/panel/playground"]'),
-        intro: '这是使用的地方，可以不需要手动设置令牌.',
-      },
-      {
-        element: document.querySelector('a[href="/panel/token"]'),
-        intro: '这是令牌，用于在其他平台使用，记得要改代理地址.',
-      }
-    ]);
+    // 获取当前页面地址
+    const currentPage = window.location.pathname;
+    let stepsData = [];
+
+    const getElementByXpath = (path) => {
+      return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    };
+
+    // 根据地址设置不同的步骤
+    switch (currentPage) {
+      case '/panel/topup':
+        stepsData = [
+          {
+            element: document.querySelector('a[href="/panel/topup"]'),
+            intro: '这是充值入口.',
+            position: 'right'
+          }
+        ];
+        break;
+      case '/panel/playground':
+        stepsData = [
+          {
+            element: document.querySelector('a[href="/panel/playground"]'),
+            intro: '这是使用的地方但是有点小，可以不需要手动设置令牌.',
+          }
+        ];
+        break;
+      case '/panel/token':
+        stepsData = [
+          {
+            element: document.querySelector('a[href="/panel/token"]'),
+            intro: '这是令牌，创建令牌后点击聊天可以在更大的界面使用，还可以用于在其他平台使用，记得要改代理地址.',
+          },
+          {
+            element: getElementByXpath('//*[@id="root"]/div/main/div/div[1]/button'),
+            intro: '点击这个可以新建令牌，里面的额度和到期时间，如果是自己用的话建议设置不限额度和永不过期噢',
+            position: 'left'
+          }
+        ];
+        break;
+      default:
+        stepsData = [
+          {
+            element: document.querySelector('a[href="/panel/playground"]'),
+            intro: '这是使用的地方但是有点小，可以不需要手动设置令牌.',
+          },
+          {
+            element: document.querySelector('a[href="/panel/token"]'),
+            intro: '这是令牌，创建令牌后点击聊天可以在更大的界面使用，还可以用于在其他平台使用，记得要改代理地址.',
+          }
+          ,
+          {
+            element: document.querySelector('a[href="/panel/midjourney"]'),
+            intro: '这是MJ绘画，画过的画会在这里出现.',
+          }
+        ];
+    }
+
+    setSteps(stepsData);
   }, [tutorialEnabled]);
 
   const handleAvatarClick = () => {
-    setTutorialEnabled(true); // 原神启动！！！
+    setTutorialEnabled(true); // 启动教程
   };
 
   const handleExit = () => {
