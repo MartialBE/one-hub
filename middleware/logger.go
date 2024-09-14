6ae1177ce8c1fd3da4b28447e5f300120aa5bd7f
wood <wood@czl.net>
2024-09-14 16:29:45 +0800

    删除无用参

--- middleware/logger.go
+++ middleware/logger.go
@@ -20,48 +20,7 @@ func GinzapWithConfig() gin.HandlerFunc {
 		start := time.Now()
 		path := c.Request.URL.Path
 		query := c.Request.URL.RawQuery
-		
-		// 获取所有请求参数
-		var requestParams map[string]interface{}
-		
-		// 获取 URL 查询参数
-		queryParams := c.Request.URL.Query()
-		if len(queryParams) > 0 {
-			requestParams = make(map[string]interface{})
-			for key, values := range queryParams {
-				if len(values) > 1 {
-					requestParams[key] = values
-				} else {
-					requestParams[key] = values[0]
-				}
-			}
-		}
-
-		// 获取 POST 表单数据
-		if c.Request.Method == "POST" || c.Request.Method == "PUT" {
-			if err := c.Request.ParseForm(); err == nil {
-				for key, values := range c.Request.PostForm {
-					if len(values) > 1 {
-						requestParams[key] = values
-					} else {
-						requestParams[key] = values[0]
-					}
-				}
-			}
-		}
-
-		// 获取 JSON 数据
-		var jsonParams map[string]interface{}
-		if c.Request.Header.Get("Content-Type") == "application/json" {
-			if err := c.ShouldBindJSON(&jsonParams); err == nil {
-				for key, value := range jsonParams {
-					requestParams[key] = value
-				}
-			}
-		}
-
 		c.Next()
-
 		end := time.Now()
 		latency := end.Sub(start)
 		requestID := c.GetString(logger.RequestIdKey)
@@ -82,10 +41,10 @@ func GinzapWithConfig() gin.HandlerFunc {
 			zap.Int("token_id", c.GetInt("token_id")),
 			zap.String("token_name", c.GetString("token_name")),
 			zap.Int("channel_id", c.GetInt("channel_id")),
-			zap.Any("request_params", requestParams), // 添加请求参数
 		}
 
 		if len(c.Errors) > 0 {
+			// Append error field if this is an erroneous request.
 			for _, e := range c.Errors.Errors() {
 				logger.Logger.Error(e, fields...)
 			}
@@ -94,4 +53,3 @@ func GinzapWithConfig() gin.HandlerFunc {
 		}
 	}
 }
-
