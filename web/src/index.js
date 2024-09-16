3b6878823c6317267320e3a9676f598e49d7e888
wood <wood@czl.net>
2024-09-16 15:55:57 +0800

    🔖 fix: 修复service worker注册错误，确保应用正常工作离线

--- web/src/index.js
+++ web/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 // project imports
-import * as serviceWorker from 'serviceWorker';
+import * as serviceWorker from 'public/serviceWorker';
 import App from 'App';
 import { store } from 'store';
 
@@ -28,4 +28,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
