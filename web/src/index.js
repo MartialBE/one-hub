ca9700cfa56e88d54e164c2506a3ded597b695c7
wood <wood@czl.net>
2024-09-16 15:53:40 +0800

    🔖 fix: 修复service worker注册错误，确保应用正常工作离线

--- web/src/index.js
+++ web/src/index.js
@@ -28,4 +28,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
