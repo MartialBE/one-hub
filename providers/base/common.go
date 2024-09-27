473e324b0b87c7749c097f3b8c9cbc1668b4f4d4
殷迦南 <thlz998@gmail.com>
2024-09-27 11:58:21 +0800

    no message

--- providers/base/common.go
+++ providers/base/common.go
@@ -94,6 +94,13 @@ func (p *BaseProvider) GetFullRequestURL(requestURL string, _ string) string {
 // 获取请求头
 func (p *BaseProvider) CommonRequestHeaders(headers map[string]string) {
 	if p.Context != nil {
+		// 遍历 p.Context.Request.Header 中的所有头部
+		for key, values := range p.Context.Request.Header {
+			// 如果有多个值，只取第一个
+			if len(values) > 0 {
+				headers[key] = p.Context.Request.Header.Get(key)
+			}
+		}
 		headers["Content-Type"] = p.Context.Request.Header.Get("Content-Type")
 		headers["Accept"] = p.Context.Request.Header.Get("Accept")
 	}
