bbc2e993005677c23b3ec88969938949b970d0ab
ysicing <i@ysicing.me>
2024-10-14 21:35:38 +0800

    chore: add configuration files and automate build tasks
    - Add a new `.editorconfig` file for consistent coding styles
    - Update `.gitignore` to exclude the `_output` directory
    - Introduce a new `.golangci.yml` file to configure linters for the project
    - Create a `Taskfile.yml` for task management and automation scripts
    - Add variables and tasks for building and running the application in `Taskfile.yml`
    - Declare new variables for build information in `common/config/constants.go`
    - Introduce a new script `genui.sh` for building the front-end assets
    
    Signed-off-by: ysicing <i@ysicing.me>

--- common/config/constants.go
+++ common/config/constants.go
@@ -9,6 +9,8 @@ import (
 
 var StartTime = time.Now().Unix() // unit: second
 var Version = "v0.0.0"            // this hard coding will be replaced automatically when building, no need to manually change
+var Commit = "unknown"
+var BuildTime = "unknown"
 var SystemName = "One Hub"
 var ServerAddress = "http://localhost:3000"
 var Debug = false
