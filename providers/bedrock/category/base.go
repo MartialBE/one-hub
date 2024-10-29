0c28d6adbc3da05928a18de7a904c10b985cbc69
ZeroDeng <denglin0105@vip.qq.com>
2024-10-29 11:18:53 +0800

    feat(Provider aws bedrock): add claude-3-5-sonnet-20241022-v2:0
    aws添加claude-3.5-sonnet v2支持

--- providers/bedrock/category/base.go
+++ providers/bedrock/category/base.go
@@ -35,6 +35,7 @@ func GetCategory(modelName string) (*Category, error) {
 func GetModelName(modelName string) string {
 	bedrockMap := map[string]string{
 		"claude-3-5-sonnet-20240620": "anthropic.claude-3-5-sonnet-20240620-v1:0",
+		"claude-3-5-sonnet-20241022": "anthropic.claude-3-5-sonnet-20241022-v2:0",
 		"claude-3-opus-20240229":     "anthropic.claude-3-opus-20240229-v1:0",
 		"claude-3-sonnet-20240229":   "anthropic.claude-3-sonnet-20240229-v1:0",
 		"claude-3-haiku-20240307":    "anthropic.claude-3-haiku-20240307-v1:0",
