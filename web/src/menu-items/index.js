66a21423a70b6d28691bd1a79448a6cd83b347b8
wood chen <wood@czl.net>
2024-11-01 03:29:26 +0800

    refactor(menu-items): update menu items structure and imports

--- web/src/menu-items/index.js
+++ web/src/menu-items/index.js
@@ -1,29 +1,29 @@
-// import Setting from './setting';
-// import Dashboard from './dashboard';
-// import Billing from './billing';
-// import usage from './usage';
-// // ==============================|| MENU ITEMS ||============================== //
-
-// const menuItems = {
-//   items: [Dashboard, Setting, Billing, usage]
-// };
-
-// export default menuItems;
-import panel from './panel';
-
+import Setting from './setting';
+import Dashboard from './dashboard';
+import Billing from './billing';
+import usage from './usage';
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [panel],
-  urlMap: {}
+  items: [Dashboard, Setting, Billing, usage]
 };
 
-// Initialize urlMap
-menuItems.urlMap = menuItems.items.reduce((map, item) => {
-  item.children.forEach((child) => {
-    map[child.url] = child;
-  });
-  return map;
-}, {});
-
 export default menuItems;
+// import panel from './panel';
+
+// // ==============================|| MENU ITEMS ||============================== //
+
+// const menuItems = {
+//   items: [panel],
+//   urlMap: {}
+// };
+
+// // Initialize urlMap
+// menuItems.urlMap = menuItems.items.reduce((map, item) => {
+//   item.children.forEach((child) => {
+//     map[child.url] = child;
+//   });
+//   return map;
+// }, {});
+
+// export default menuItems;
