8f59cfa06fc29be861459e365e21a2972113e6e6
ZeroDeng <denglin0105@vip.qq.com>
2024-11-04 15:34:27 +0800

    feat(i18n): 增加繁体中文支持 (#406)
    * feat(i18n): 增加繁体中文支持
    
    增加繁体中文支持
    #405
    
    * fix(setting i18n style): 修复设置页面alert换行问题
    
    修复设置页面因为国际化适配导致部分alert内容无法换行问题

--- web/.i18nrc.js
+++ web/.i18nrc.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   entry: 'src/i18n/locales/zh_CN.json',
   entryLocale: 'zh_CN',
   output: 'src/i18n/locales',
-  outputLocales: ['en_US', 'ja_JP'],
+  outputLocales: ['en_US', 'ja_JP','zh_HK'],
   temperature: 0,
-  modelName: 'gpt-3.5-turbo',
+  modelName: 'gpt-4o-mini',
   splitToken: 2048,
   experimental: {
     jsonMode: true
--- web/src/i18n/i18nList.js
+++ web/src/i18n/i18nList.js
@@ -1,5 +1,6 @@
 const i18nList = [
-  { lng: 'zh_CN', name: '中文' },
+  { lng: 'zh_CN', name: '简体中文' },
+  { lng: 'zh_HK', name: '繁體中文' },
   { lng: 'en_US', name: 'English' },
   { lng: 'ja_JP', name: '日本語' }
 ];
