a4f735297c103483ef613e5c504087f75c0e8dae
MartialBE <me@xiao5.info>
2025-04-11 22:10:55 +0800

    feat(azure): 在配置中添加响应路径(#579)
    - 在 Azure 配置中新增 Responses 路径，增强功能扩展性。

--- providers/azure/base.go
+++ providers/azure/base.go
@@ -38,6 +38,7 @@ func getAzureConfig() base.ProviderConfig {
 		ImagesGenerations:   "/images/generations",
 		ChatRealtime:        "/realtime",
 		ModelList:           "isGetAzureModelList", // 在azure中该参数不参与实际url拼接，只是起到flag的作用
+		Responses:           "/responses",
 	}
 }
 
