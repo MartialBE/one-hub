612cda6fa3f13a0f88eadf1514e40d3824cb740b
MartialBE <me@xiao5.info>
2025-04-24 18:10:42 +0800

    feat(image): 支持处理多个图像文件，更新ImageEditRequest结构体以包含Images字段

--- providers/openai/image_edits.go
+++ providers/openai/image_edits.go
@@ -85,10 +85,21 @@ func (p *OpenAIProvider) getRequestImageBody(relayMode int, ModelName string, re
 	return req, nil
 }
 
-func imagesEditsMultipartForm(request *types.ImageEditRequest, b requester.FormBuilder) error {
-	err := b.CreateFormFile("image", request.Image)
-	if err != nil {
-		return fmt.Errorf("creating form image: %w", err)
+func imagesEditsMultipartForm(request *types.ImageEditRequest, b requester.FormBuilder) (err error) {
+	if request.Image != nil {
+		err = b.CreateFormFile("image", request.Image)
+		if err != nil {
+			return fmt.Errorf("creating form image: %w", err)
+		}
+	}
+
+	if request.Images != nil {
+		for _, image := range request.Images {
+			err = b.CreateFormFile("image[]", image)
+			if err != nil {
+				return fmt.Errorf("creating form images: %w", err)
+			}
+		}
 	}
 
 	err = b.WriteField("prompt", request.Prompt)
