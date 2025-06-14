2c1da35bd396ea0c2daa2d72c7708e76639cdd90
ZeroDeng <denglin0105@vip.qq.com>
2025-06-14 11:14:48 +0800

    refactor(router): 简化适配低版本gin嵌入文件系统的初始化逻辑
    移除了`EmbedFolder`函数的错误处理逻辑，直接返回嵌入文件系统对象。优化后的代码减少了不必要的错误判断，提升了可读性和维护性。

--- router/web-router.go
+++ router/web-router.go
@@ -18,11 +18,7 @@ func SetWebRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
 	// 特别处理favicon.ico请求
 	router.GET("/favicon.ico", controller.Favicon(buildFS))
 
-	embedFS, err := static.EmbedFolder(buildFS, "web/build")
-	if err != nil {
-		// 处理错误，可以选择记录日志或者 panic
-		panic("无法创建嵌入文件系统: " + err.Error())
-	}
+	embedFS := static.EmbedFolder(buildFS, "web/build")
 	router.Use(static.Serve("/", embedFS))
 
 	router.NoRoute(func(c *gin.Context) {
