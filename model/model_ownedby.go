9e7fd00b6465db0f86d40b05a4bb25627c5dfa47
ZeroDeng <denglin0105@vip.qq.com>
2025-09-02 10:40:01 +0800

    fix(channel): 修正XAI渠道名称大小写问题
    - 更新`ChannelTypeXAI`渠道名称为`xAI`，使其符合命名规范。
    - 确保图标配置地址不受名称更改影响。

--- model/model_ownedby.go
+++ model/model_ownedby.go
@@ -199,6 +199,6 @@ func GetDefaultModelOwnedBy() []*ModelOwnedBy {
 		{Id: config.ChannelTypeRecraft, Name: "RecraftAI", Icon: "https://registry.npmmirror.com/@lobehub/icons-static-svg/latest/files/icons/recraft.svg"},
 		{Id: config.ChannelTypeKling, Name: "Kling", Icon: "https://registry.npmmirror.com/@lobehub/icons-static-svg/latest/files/icons/kling-color.svg"},
 		{Id: config.ChannelTypeOpenRouter, Name: "OpenRouter", Icon: "https://registry.npmmirror.com/@lobehub/icons-static-svg/latest/files/icons/openrouter.svg"},
-		{Id: config.ChannelTypeXAI, Name: "XAI", Icon: "https://registry.npmmirror.com/@lobehub/icons-static-webp/1.24.0/files/light/xai.webp"},
+		{Id: config.ChannelTypeXAI, Name: "xAI", Icon: "https://registry.npmmirror.com/@lobehub/icons-static-webp/1.24.0/files/light/xai.webp"},
 	}
 }
